6efa653106c0a439790b60f35201fc0103da7e47
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2022-09-29 21:16:52 +0400

    Initial commit

---
+++ src/main/java/com/mulberry/blackmulberry/BlackMulberryApplication.java
@@ -0,0 +1,13 @@
+package com.mulberry.blackmulberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackMulberryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackMulberryApplication.class, args);
+	}
+
+}
