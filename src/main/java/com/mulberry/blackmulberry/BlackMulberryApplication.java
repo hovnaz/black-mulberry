65ff55a399aacef28d8a5cdb9a92c7783d5d6a43
Sevak <sevakmarti@gmail.com>
2022-10-03 18:26:34 +0400

    probels

--- src/main/java/com/mulberry/blackmulberry/BlackMulberryApplication.java
+++ src/main/java/com/mulberry/blackmulberry/BlackMulberryApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlackMulberryApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlackMulberryApplication.class, args);
 	}
