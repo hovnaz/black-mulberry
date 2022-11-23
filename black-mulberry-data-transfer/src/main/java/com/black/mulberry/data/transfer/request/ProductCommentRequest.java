7905a3ecc4fa4b242109dfec86450bb942585611
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2022-11-24 01:48:52 +0400

    fixed building issue

--- black-mulberry-data-transfer/src/main/java/com/black/mulberry/data/transfer/request/ProductCommentRequest.java
+++ black-mulberry-data-transfer/src/main/java/com/black/mulberry/data/transfer/request/ProductCommentRequest.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Range;
-
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
