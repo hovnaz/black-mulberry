41844f4ca26ad86bb692deda51eca999e84eb9d0
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2022-12-09 21:08:21 +0400

    add endpoint and validation

--- black-mulberry-data-transfer/src/main/java/com/black/mulberry/data/transfer/request/ProductBasketItemRequest.java
+++ black-mulberry-data-transfer/src/main/java/com/black/mulberry/data/transfer/request/ProductBasketItemRequest.java
@@ -5,12 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class ProductBasketItemRequest {
 
+    @Min(1)
     private int quantity;
+    @Min(1)
     private long productId;
 }
--- black-mulberry-rest/src/main/java/com/black/mulberry/rest/endpoint/ProductBasketEndpoint.java
+++ black-mulberry-rest/src/main/java/com/black/mulberry/rest/endpoint/ProductBasketEndpoint.java
@@ -1,87 +1,66 @@
 package com.black.mulberry.rest.endpoint;
 
-import com.black.mulberry.core.mapper.ProductBasketMapper;
 import com.black.mulberry.core.security.CurrentUser;
 import com.black.mulberry.core.service.ProductBasketService;
 import com.black.mulberry.data.transfer.request.ProductBasketItemRequest;
 import com.black.mulberry.data.transfer.response.ProductBasketItemResponse;
-import com.black.mulberry.data.transfer.response.ProductBasketResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/product/basket")
+@PreAuthorize("isAuthenticated()")
 public class ProductBasketEndpoint {
 
     private final ProductBasketService productBasketService;
-    private final ProductBasketMapper productBasketMapper;
 
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping("/user")
-//    public ResponseEntity<List<ProductCommentResponse>> findPaginatedByUserId(
-//            @RequestParam("user_id") long userId,
-//            @PageableDefault Pageable pageable) {
-//        return ResponseEntity.ok(productCommentService.findPaginatedByUserId(userId, pageable));
-//    }
-
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping("/user/count")
-//    public ResponseEntity<Long> countAllPaginatedByUserId(
-//            @RequestParam("user_id") long userId) {
-//        return ResponseEntity.ok(productCommentService.countAllByUserId(userId));
-//    }
-//
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping("/user_and_product")
-//    public ResponseEntity<List<ProductCommentResponse>> findPaginatedByProductIdAndUserId(
-//            @RequestParam("user_id") long userId,
-//            @RequestParam("product_id") long productId,
-//            @PageableDefault Pageable pageable) {
-//        return ResponseEntity.ok(productCommentService.findPaginatedByProductIdAndUserId(productId, userId, pageable));
-//    }
-
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping("/user_and_product/count")
-//    public ResponseEntity<Long> countAllByProductIdAndUserId(
-//            @RequestParam("user_id") long userId,
-//            @RequestParam("product_id") long productId) {
-//        return ResponseEntity.ok(productCommentService.countAllByProductIdAndUserId(productId, userId));
-//    }
-//
-//    @GetMapping("/product")
-//    public ResponseEntity<List<ProductCommentResponse>> findPaginatedByProductId(
-//            @RequestParam("product_id") long productId,
-//            @PageableDefault Pageable pageable) {
-//        return ResponseEntity.ok(productCommentService.findPaginatedByProductId(productId, pageable));
-//    }
-//
-//    @GetMapping("/product/count")
-//    public ResponseEntity<Long> countAllByProductId(@RequestParam("product_id") long productId) {
-//        return ResponseEntity.ok(productCommentService.countAllByProductId(productId));
-//    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductBasketResponse> findById(
-            @PathVariable long id) {
-        ProductBasketResponse productBasketResponse = productBasketMapper.toResponse(productBasketService.findById(id));
-        return ResponseEntity.ok(productBasketResponse);
+    @GetMapping
+    public ResponseEntity<List<ProductBasketItemResponse>> findAll(@PageableDefault Pageable pageable,
+                                                                   @AuthenticationPrincipal CurrentUser currentUser) {
+        return ResponseEntity.ok(productBasketService.findAllByActual(currentUser.getId(), pageable));
     }
 
-    @PreAuthorize("isAuthenticated()")// +
+    @GetMapping
+    public ResponseEntity<BigDecimal> amount(@AuthenticationPrincipal CurrentUser currentUser) {
+        return ResponseEntity.ok(productBasketService.amountByUserId(currentUser.getId()));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@Valid @RequestBody ProductBasketItemRequest productBasketItemRequest,
+                                    @AuthenticationPrincipal CurrentUser currentUser) {
+        return ResponseEntity.ok(productBasketService.update(productBasketItemRequest, currentUser.getId()));
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<ProductBasketResponse> deleteById(@PathVariable long id, @AuthenticationPrincipal CurrentUser currentUser) {
-        ProductBasketResponse productBasketResponse = productBasketService.cancelByProductId(id, currentUser.getId());
-        return ResponseEntity.ok(productBasketResponse);
+    public ResponseEntity<Long> cansel(@PathVariable long id, @AuthenticationPrincipal CurrentUser currentUser) {
+        productBasketService.cancelByProductId(currentUser.getId(), id);
+        return ResponseEntity.ok(id);
     }
 
-    @PreAuthorize("isAuthenticated()")// +
-    @PostMapping("/")
+    @DeleteMapping
+    public ResponseEntity<?> clear(@AuthenticationPrincipal CurrentUser currentUser) {
+        productBasketService.clear(currentUser.getId());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
     public ResponseEntity<ProductBasketItemResponse> add(@Valid @RequestBody ProductBasketItemRequest productBasketItemRequest,
                                                          @AuthenticationPrincipal CurrentUser currentUser) {
         ProductBasketItemResponse productBasketItemResponse = productBasketService.add(productBasketItemRequest, currentUser.getId());
