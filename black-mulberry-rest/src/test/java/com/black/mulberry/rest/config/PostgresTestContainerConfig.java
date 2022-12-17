f68be69b92877b1578d25916b804c9b4a7fe6916
Sevak <sevakmarti@gmail.com>
2022-12-17 15:10:46 +0400

    unit test for service

--- black-mulberry-rest/src/test/java/com/black/mulberry/rest/config/PostgresTestContainerConfig.java
+++ black-mulberry-rest/src/test/java/com/black/mulberry/rest/config/PostgresTestContainerConfig.java
@@ -2,21 +2,4 @@ package com.black.mulberry.rest.config;
 
 //@Testcontainers
 public class PostgresTestContainerConfig {
-
-//    private static final String DB_SCHEMA = "test";
-//    private static final String DB_USERNAME = "postgres";
-//    private static final String DB_PASSWORD = "root";
-//
-//    @Container
-//    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:13")
-//            .withDatabaseName(DB_SCHEMA)
-//            .withUsername(DB_USERNAME)
-//            .withPassword(DB_PASSWORD);
-//
-//    @DynamicPropertySource
-//    static void properties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-//        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-//        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-//    }
 }
